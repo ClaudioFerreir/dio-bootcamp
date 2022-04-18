36a2129c4a089635357f57f80afbb57182f9bdc3
claudioferreira <claudioferreirajr@hotmail.com>
2022-04-17 21:35:06 -0300

    dark mode complete

--- manipulando_o_DOM/assets/js/scripts.js
+++ manipulando_o_DOM/assets/js/scripts.js
@@ -1,15 +1,31 @@
 function changeMode() {
 //console.log('clickei!'); para testar se estar funcionando o click
 changeClasses();
+changeText();
 }
 
 function changeClasses() {
-  button.classList.toggle('dark-mode');
-  h1.classList.toggle('dark-mode');
-  body.classList.toggle('dark-mode');
-  footer.classList.toggle('dark-mode');
+  button.classList.toggle(darkModeClass);
+  h1.classList.toggle(darkModeClass);
+  body.classList.toggle(darkModeClass);
+  footer.classList.toggle(darkModeClass);
 }
 
+function changeText() {
+  const lightMode = "Light Mode";
+  const darkMode = "Dark Mode";
+
+  if(body.classList.contains(darkModeClass)) {
+    button.innerHTML = lightMode;
+    h1.innerHTML = darkMode + " ON";
+    return
+  }
+
+  button.innerHTML = darkMode;
+  h1.innerHTML = lightMode + " ON";
+}
+
+darkModeClass = 'dark-mode';
 const button = document.getElementById('mode-selector');
 // console.log(button); verificar se o botao esta selecionado
 const h1 = document.getElementById('page-title');
